1a21432f45ae1e1845c984e61f3ea5ef5e9f02e9
graevsky <graevsky@bk.ru>
2025-05-05 21:57:05 +0300

    Fix formatting

--- src/test/kotlin/org/example/actions/NoteActionTest.kt
+++ src/test/kotlin/org/example/actions/NoteActionTest.kt
@@ -2,19 +2,8 @@ package org.example.actions
 
 import java.net.InetAddress
 import kotlin.test.*
-import kotlinx.coroutines.runBlocking
-import kotlinx.datetime.LocalDateTime
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.example.dto.DbCreateRequest
-import org.example.dto.DbNoteRow
-import org.example.dto.DbResponse
-import org.example.model.Note
 import org.junit.jupiter.api.*
-import org.junit.jupiter.api.Test
 
 class NoteActionTest {
   private lateinit var server: MockWebServer
--- src/test/kotlin/org/example/actions/NotificationActionTest.kt
+++ src/test/kotlin/org/example/actions/NotificationActionTest.kt
@@ -1,24 +1,7 @@
 package org.example.actions
 
 import java.net.InetAddress
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertNull
-import kotlin.test.assertTrue
-import kotlinx.coroutines.runBlocking
-import kotlinx.datetime.LocalDateTime
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.example.dto.DbCreateRequest
-import org.example.dto.DbNotificationRow
-import org.example.dto.DbResponse
-import org.example.model.Notification
-import org.example.model.NotificationActionEnum
-import org.example.model.NotificationSnoozeEnum
 import org.junit.jupiter.api.*
 
 class NotificationActionTest {
